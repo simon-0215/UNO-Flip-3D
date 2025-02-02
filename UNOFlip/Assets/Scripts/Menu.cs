1fed0fd7b894eb49eb8a90fb02317793bf8f8c58
Mingyang Xu <122560142+simon-0215@users.noreply.github.com>
2025-02-02 17:18:39 -0500

    implement the host and client button logic and clear up all the bugs related on multiplayer and still need to fix the game logic

--- UNOFlip/Assets/Scripts/Menu.cs
+++ UNOFlip/Assets/Scripts/Menu.cs
@@ -1,20 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField]  GameObject howToPlayPanel;
+    [SerializeField] private GameObject howToPlayPanel;
 
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
     }
 
+    public void HostGame()
+    {
+        if (NetworkManager.singleton != null)
+        {
+            NetworkManager.singleton.StartHost();
+        }
+        else
+        {
+            Debug.LogError("NetworkManager.singleton is null. Ensure there is a NetworkManager in the scene.");
+        }
+    }
+
+    public void JoinGame()
+    {
+        if (NetworkManager.singleton != null)
+        {
+            NetworkManager.singleton.StartClient();
+        }
+        else
+        {
+            Debug.LogError("NetworkManager.singleton is null. Ensure there is a NetworkManager in the scene.");
+        }
+    }
+
     public void HowToPlayPanel()
-    {  
-        if(howToPlayPanel != null)
+    {
+        if (howToPlayPanel != null)
         {
             howToPlayPanel.SetActive(true);
         }
